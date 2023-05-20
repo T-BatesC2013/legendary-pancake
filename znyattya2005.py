b092d04157626b9ca3cd0d3ddce7ba359e9ad7a3
PasksRan1 <sododomshag@gmail.com>
2023-05-20 09:51:23 +0300

    20 05 commit

--- znyattya2005.py
+++ znyattya2005.py
@@ -16,7 +16,7 @@ nick=Pupil()
 kate=Pupil(height=170)
 print(nick.height)
 print(kate.height)'''
-class Student:
+'''class Student:
     amount_of_students=0
     def __init__(self, height=172):
         self.height=height
@@ -27,4 +27,27 @@ print(nick.height)
 print(nick.amount_of_students)
 kate=Student(height=170)
 print(kate.height)
-print(kate.amount_of_students)
+print(kate.amount_of_students)'''
+'''class Student:
+    height=160
+    def __init__(self):
+        print(self.height)
+        Student.height+=10
+nick=Student()
+kate=Student()'''
+class Student:
+    amount_of_students=0
+    def __init__(self, height=160):
+        self.height=height
+        Student.amount_of_students+=1
+    def grow(self, height=1):
+        self.height+=height
+nick=Student()
+print(nick.height)
+nick.grow(height=15)
+print(nick.height)
+
+kate=Student(150)
+print(kate.height)
+kate.grow(5)
+print(kate.height)
